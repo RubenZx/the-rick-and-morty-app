dfba8c9f8cd5fe436b2ede2c5387e741c8a95e9a
Rubén Montero Domínguez <ruben.montero@opinno.com>
2020-03-03 12:30:59 +0100

    feat: layout for our application

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,45 @@
+import {
+  AppBar,
+  fade,
+  Grid,
+  InputBase,
+  Toolbar,
+  Typography,
+} from '@material-ui/core'
+import { Search } from '@material-ui/icons'
+import React from 'react'
+import styled from 'styled-components'
+
+const SearchBar = styled.div`
+  position: relative;
+  border-radius: ${({ theme }) => theme.shape.borderRadius};
+  background-color: ${({ theme }) => fade(theme.palette.common.white, 0.15)};
+  &:hover {
+    background-color: ${({ theme }) => fade(theme.palette.common.white, 0.25)};
+  }
+  margin-left: 0;
+  width: 100%;
+  ${({ theme }) => theme.breakpoints.up('sm')} {
+    margin-left: ${({ theme }) => theme.spacing(1)};
+    width: auto;
+  }
+`
+
+export default () => (
+  <AppBar position="static">
+    <Toolbar>
+      <Grid container>
+        <Grid item xs>
+          <Typography variant="h6">The Rick and Morty App</Typography>
+        </Grid>
+        <SearchBar>
+          <InputBase
+            startAdornment={<Search />}
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </SearchBar>
+      </Grid>
+    </Toolbar>
+  </AppBar>
+)
