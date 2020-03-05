c8029a15510f697e23fe064f205f725a882d3834
JaimeJesúsSerrano <jaimejesusserrano@gmail.com>
2020-03-05 17:07:48 +0100

    feat: css main structure

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,16 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
+import Breadcrumbs from './Breadcrumbs'
 import Footer from './footer'
-import Header from './header'
+import Menu from './menu'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
+const StyledChildren = styled.div
+`
+  background-color: ${({ theme }) => theme.palette.primary.light};
+  display: flex;
+  flex: 1 0 auto;
+  padding: 5px 15px 5px 15px;
+`
+
+const StyledFooter = styled.div
+`
+  background-color: ${({ theme }) => theme.palette.secondary.main};
+  display: flex;
+  flex-shrink: 0;
+  min-height: 100px;
+  padding: 5px 15px 5px 15px;
+`
+
+const StyledBreadcrumb = styled.div
+`
+  background-color: ${({ theme }) => theme.palette.primary.light};
+  display: flex;
+  padding: 10px 15px 5px 15px;
+`
+
+
 const Layout = ({ children }: LayoutProps) => (
   <>
-    <Header />
-    {children}
-    <Footer />
+    <Menu />
+    <StyledBreadcrumb>
+      <Breadcrumbs />
+    </StyledBreadcrumb>
+    <StyledChildren>
+      {children}
+    </StyledChildren>
+    <StyledFooter>
+      <Footer />
+    </StyledFooter>
   </>
 )
 
