c07164af3d7fa58ae57d682bec83f2918731aafd
JaimeJesúsSerrano <jaimejesusserrano@gmail.com>
2020-03-05 17:35:58 +0100

    feat: sidebar

---
+++ src/components/layout/Sidebar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+      <h1>Episodes</h1>
+    </>
+  )
+}
+
+export default Sidebar
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,21 +3,39 @@ import styled from 'styled-components'
 import Breadcrumbs from './Breadcrumbs'
 import Footer from './footer'
 import Menu from './menu'
+import Sidebar from '~Components/layout/Sidebar'
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
-const StyledChildren = styled.div
-`
+const StyledBreadcrumb = styled.div`
   background-color: ${({ theme }) => theme.palette.primary.light};
   display: flex;
+  padding: 10px 15px 5px 15px;
+`
+
+const StyledContent = styled.div`
+  display: flex;
   flex: 1 0 auto;
+  flex-direction: row;
+`
+
+const StyledChildren = styled.div`
+  background-color: ${({ theme }) => theme.palette.primary.light};
+  display: flex;
+  flex: 8 0 auto;
   padding: 5px 15px 5px 15px;
 `
 
-const StyledFooter = styled.div
+const StyledSidebar = styled.div`
+  background-color: ${({ theme }) => theme.palette.secondary.main};
+  display: flex;
+  flex: 2;
+  padding: 5px 15px 50px 15px;
 `
+
+const StyledFooter = styled.div`
   background-color: ${({ theme }) => theme.palette.secondary.main};
   display: flex;
   flex-shrink: 0;
@@ -25,23 +43,21 @@ const StyledFooter = styled.div
   padding: 5px 15px 5px 15px;
 `
 
-const StyledBreadcrumb = styled.div
-`
-  background-color: ${({ theme }) => theme.palette.primary.light};
-  display: flex;
-  padding: 10px 15px 5px 15px;
-`
-
-
 const Layout = ({ children }: LayoutProps) => (
   <>
     <Menu />
+
     <StyledBreadcrumb>
       <Breadcrumbs />
     </StyledBreadcrumb>
-    <StyledChildren>
-      {children}
-    </StyledChildren>
+
+    <StyledContent>
+      <StyledChildren>{children}</StyledChildren>
+      <StyledSidebar>
+        <Sidebar />
+      </StyledSidebar>
+    </StyledContent>
+
     <StyledFooter>
       <Footer />
     </StyledFooter>
