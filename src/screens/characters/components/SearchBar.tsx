8678ed7e02c3251cb62bc7c12a253359ec752f17
Rubén Montero Domínguez <ruben.montero@opinno.com>
2020-03-06 14:01:46 +0100

    feat: search bar on characters screen

--- src/screens/characters/components/SearchBar.tsx
+++ src/screens/characters/components/SearchBar.tsx
@@ -1,5 +1,17 @@
-import { TextField } from '@material-ui/core'
+import { fade, Grid, InputBase } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 import React from 'react'
+import styled from 'styled-components'
+
+const SearchContainer = styled(Grid)`
+  margin-top: 15px;
+  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
+  height: 50px;
+  background-color: ${({ theme }) => fade(theme.palette.common.white, 0.25)};
+  &:hover {
+    background-color: ${({ theme }) => fade(theme.palette.common.white, 0.5)};
+  }
+`
 
 interface SearchBarProps {
   element: string
@@ -7,7 +19,20 @@ interface SearchBarProps {
 }
 const SearchBar = ({ element, handleChange }: SearchBarProps) => {
   return (
-    <TextField value={element} onChange={e => handleChange(e.target.value)} />
+    <SearchContainer container justify="space-around" alignItems="center">
+      <Grid container item xs={1} direction="column" alignItems="center">
+        <SearchIcon />
+      </Grid>
+      <Grid item xs={11}>
+        <InputBase
+          fullWidth
+          placeholder="Search a character"
+          value={element}
+          onChange={e => handleChange(e.target.value)}
+          inputProps={{ 'aria-label': 'search' }}
+        />
+      </Grid>
+    </SearchContainer>
   )
 }
 
