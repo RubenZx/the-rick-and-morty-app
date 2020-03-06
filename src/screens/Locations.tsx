27b56db6593a9440952a5f160a163c2d6a22b4fb
Rubén Montero Domínguez <ruben.montero@opinno.com>
2020-03-06 14:02:59 +0100

    feat: added titles on all screens

--- src/screens/Locations.tsx
+++ src/screens/Locations.tsx
@@ -1,9 +1,15 @@
+import { Grid } from '@material-ui/core'
 import React from 'react'
+import { ReactComponent as Title } from '~Assets/img/locations_title.svg'
 
 const Locations = () => {
-    return (
-        <h1>Locations</h1>
-    )
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Title />
+      </Grid>
+    </Grid>
+  )
 }
 
 export default Locations
